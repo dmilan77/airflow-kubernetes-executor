64c5bdf03a3ebb0178c1fca976c5662686c98cff
Milan Das <milan@ulap.co>
2020-04-26 10:58:17 -0400

    2020-04-26T14:58:17Z

--- bin/apply-all.sh
+++ bin/apply-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPTDIR}/init.sh
-
+kubectl apply -f ${SCRIPTDIR}/../yaml/namespace.yaml
 akubectl apply -f ${SCRIPTDIR}/../yaml/
